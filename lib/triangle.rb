123ff416f838c51ce3c30929a596ead01c7bc986
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-04-11 17:24:01 -0500

    commit tree 2c68beb88343671e9020a34b2a14a736390799c2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    [side_a, side_b, side_c].each do |side|
+      Triangle.validate_side(side)
+    end
+    Triangle.validate_inequality(side_a, side_b, side_c)
+
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if side_a == side_b and side_a == side_c
+      :equilateral
+    elsif side_a != side_b and
+      side_a != side_c and
+      side_b != side_c
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  private
+  def self.validate_side(length)
+    if length <= 0
+      raise TriangleError
+    end
+  end
+
+  def self.validate_inequality(a, b, c)
+    unless a + b > c and b + c > a and a + c > b
+      raise TriangleError
+    end
+  end
+
+  public
+  class TriangleError < StandardError
+
+  end
 end
